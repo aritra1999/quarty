4acf75ea49bd5bb6290a2ffa23cee515d1d0295d
hrishavjha <hrishav@chaosnative.com>
2022-02-12 18:01:26 +0530

    Added Chat Page

--- quarty/settings.py
+++ quarty/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-bdqrrk=evw!xvt7)ow^h$3vj1#k!o6kq#d$c9cp@#s37acpz4i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -37,19 +38,16 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    #Social Auth
+
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
     'allauth.socialaccount.providers.google',
-    # Django channel
-    'channels',
-    # Project apps
+
     'accounts',
     'chat',
     'dashboard',
     'recomendations'
-
 ]
 
 MIDDLEWARE = [
@@ -80,7 +78,6 @@ TEMPLATES = [
     },
 ]
 
-ASGI_APPLICATION = 'quarty.asgi.application'
 WSGI_APPLICATION = 'quarty.wsgi.application'
 
 
@@ -92,16 +89,6 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}
-
-CHANNEL_LAYERS = {
-    "default": {
-        'BACKEND': 'channels_redis.core.RedisChannelLayer',
-        'CONFIG': {
-            "hosts": [("127.0.0.1", 6379)],
-        },
- 
-    },
 }
 
 
@@ -130,6 +117,7 @@ AUTH_PASSWORD_VALIDATORS = [
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'Asia/Kolkata'
+
 USE_I18N = True
 
 USE_L10N = True
@@ -169,3 +157,5 @@ SITE_ID = 2
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "assets")]
